33fc9978f487c434e5849a0fdbce982d43674ef4
Hussein <hussendon3@gmail.com>
2024-02-26 11:38:15 +0300

    first finish

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'فلّة';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = ' فلة المتخصصة بعناية الحدائق والمنازل احجز الان , فلة تحلي بيتك, حدائق خضراء منازل جميلة';
